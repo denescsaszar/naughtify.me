d1c683726b31bb0beaa9762c526a7f4cdbc17035
Denes <csaszar.denes@gmail.com>
2023-06-01 15:36:29 +0200

    solved the cookies issue

---
+++ client/src/components/Nav.js
@@ -0,0 +1,28 @@
+import whiteLogo from "../assets/images/white-logo.png";
+import colorLogo from "../assets/images/black-logo.png";
+import qrLogo from "../assets/images/QR-logo.gif";
+
+const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false);
+  };
+
+  return (
+    <nav>
+      <div className="logo-container">
+        <img className="logo" src={qrLogo} alt="logo" />
+      </div>
+      {!authToken && !minimal && (
+        <button
+          className="nav-button"
+          onClick={handleClick}
+          disabled={showModal}
+        >
+          Log in
+        </button>
+      )}
+    </nav>
+  );
+};
+export default Nav;
