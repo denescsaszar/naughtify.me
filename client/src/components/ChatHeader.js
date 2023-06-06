5da37ed2e8c6f81e2dd437e96cd5c7fb15ba6ea5
Denes <csaszar.denes@gmail.com>
2023-06-06 11:25:18 +0200

    styling chat header

--- client/src/components/ChatHeader.js
+++ client/src/components/ChatHeader.js
@@ -18,7 +18,7 @@ const ChatHeader = ({ user }) => {
         <h3>{user.first_name}</h3>
       </div>
       <i className="log-out-icon" onClick={logout}>
-        ⇦
+        BYE BYE
       </i>
     </div>
   );
